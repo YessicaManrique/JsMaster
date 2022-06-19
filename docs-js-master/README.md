97cdebffbe21849691dfd873ee339b088c85679e
YessicaManrique <yessimb@gmail.com>
2022-06-19 03:28:48 -0500

    se modifica archivos

--- docs-js-master/README.md
+++
@@ -1,2 +0,0 @@
-# docs-js
-El sguientes proyecto esta pensado para dar soporte a alumnoes en temas de js . segmentando en lineas de codigo cada tema y ejemplo
--- docs-js-master/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("mostrar-menu").addEventListener("click",function(){
-  
-    document.getElementById("menu").classList.toggle("mostrar");
-  });
